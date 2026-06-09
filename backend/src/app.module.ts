df861755d01cf179667e265b9782594ae2723b4c
manumonse305-hash <manumonse305@gmail.com>
2026-06-09 01:17:29 -0400

    Update app.module.ts
    triste

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -23,15 +23,18 @@ import { CaptchaModule } from './captcha/captcha.module';
     }),
 
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '3306'),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}),
 
     RolesModule,
     UsuariosModule,
